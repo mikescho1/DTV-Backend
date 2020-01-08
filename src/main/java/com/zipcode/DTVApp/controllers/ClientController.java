6fe4c456ab3e35a19afed247f2dced903c322300
mike <mike.schoenbeck@gmail.com>
2020-01-07 19:36:14 -0500

    updated jar file and connected dev branch to cloudfoundry

--- src/main/java/com/zipcode/DTVApp/controllers/ClientController.java
+++ src/main/java/com/zipcode/DTVApp/controllers/ClientController.java
@@ -1,7 +1,17 @@
 package com.zipcode.DTVApp.controllers;
 
+import com.zipcode.DTVApp.services.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class ClientController {
+
+    private ClientService clientService;
+
+    @Autowired
+    public ClientController(ClientService clientService) {
+        this.clientService = clientService;
+    }
 }
