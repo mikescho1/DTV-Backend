dbe17481dc8114e60f283ed906079ddc92076728
mike <mike.schoenbeck@gmail.com>
2020-01-14 18:39:56 -0500

    branch compiles, but can't push to cloudfoundry

--- src/main/java/com/zipcode/controllers/DBProfileController.java
+++ src/main/java/com/zipcode/controllers/DBProfileController.java
@@ -1,16 +1,16 @@
-package com.zipcode.controllers;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-public class DBProfileController {
-
-    @Value("${app.message}")
-    private String welcomeMessage;
-
-    @GetMapping("/welcome")
-    public String getDataBaseConnectionDetails() {
-        return welcomeMessage;
-    }
-}
+//package com.zipcode.controllers;
+//
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//public class DBProfileController {
+//
+//    @Value("${app.message}")
+//    private String welcomeMessage;
+//
+//    @GetMapping("/welcome")
+//    public String getDataBaseConnectionDetails() {
+//        return welcomeMessage;
+//    }
+//}
